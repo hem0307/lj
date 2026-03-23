3c0b90399e7cb86715a41ca2cfd2f063a8bde2d2
hem0307 <hemkhatri0307@gmail.com>
2026-03-23 10:12:27 +0530

    Add files via upload

--- http_module_P2.js
+++ http_module_P2.js
@@ -2,8 +2,14 @@ var http=require('http')
 http.createServer(function(req,res)
 {
 
-    res.writeHead(200,{"content-Type":"text/html"})
-    res.write("<h1> Hello in H1 <h1>")
+    // res.writeHead(200,{"content-Type":"text/html"})
+    // res.write("<h1> Hello in H1 <h1>")
+    // res.writeHead(200,{"content-Type":"text/plaing"})
+    // res.write("<h1> Hello in H1 <h1>")
+    res.writeHead(200,{"content-Type":"application/json"})
+    let a={Name:"abc",no:1}
+    res.write(JSON.stringify(a))
+    
     res.end()
     
 }).listen(8080)
